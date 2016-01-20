731d1ac48c502fa3882380b72292cefdb7def25f
Peihao Sun <phsun@sherlock-ln01.stanford.edu>
2016-01-20 14:32:55 -0800

    loop over directories

--- yt_plots.py
+++ yt_plots.py
@@ -1,21 +1,15 @@
 import yt
 
-for i in xrange(12):
-    filename = "DD%04i/data%04i" % (i, i)
+i = 0
+while os.path.isdir("DD%04d" % i):
+    filename = "DD%04d/data%04d" % (i, i)
     ds = yt.load(filename)
 
     ds.print_stats()
+    plot1 = yt.SlicePlot(ds, 'z', "all_cic")
+    plot1.save()
 
-    ## show particles (zoomed in)
-    #p1 = yt.ParticlePlot(ds, "particle_position_x", "particle_position_y",
-    #                    width = (0.05, 0.05), color = 'b')
-    #p1.save()
-    
-    ## show particle_mass
-    #p2 = yt.ParticlePlot(ds, "particle_position_x", "particle_position_y",
-    #                    "particle_mass", width = (1., 1.))
-    #p2.save()
+    plot2 = yt.ProjectionPlot(ds, 'z', "all_cic")
+    plot2.save()
 
-    ## density plots
-    p3 = yt.SlicePlot(ds, 'z', "all_cic")
-    p3.save()
+    i += 1
