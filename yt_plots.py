2fd663722e580d2d7aa39f994de0f8ae039035a7
Peihao Sun <phsun@sherlock-ln01.stanford.edu>
2016-01-21 13:59:08 -0800

    batch script editing

--- yt_plots.py
+++ yt_plots.py
@@ -1,11 +1,10 @@
+import os
 import yt
 
 i = 0
 while os.path.isdir("DD%04d" % i):
-    filename = "DD%04d/data%04d" % (i, i)
-    ds = yt.load(filename)
+    ds = yt.load("DD%04d/data%04d" % (i, i))
 
-    ds.print_stats()
     plot1 = yt.SlicePlot(ds, 'z', "all_cic")
     plot1.save()
 
