653336a2b9fb8f689d65943a2b2b10fbcfd334fd
Peihao Sun <phsun@sherlock-ln01.stanford.edu>
2016-01-27 21:17:44 -0800

    sbatch job name change

--- run_music_and_enzo_256.sh
+++ run_music_and_enzo_256.sh
@@ -16,6 +16,8 @@ mkdir 256_$c
 cd 256_$c
 cp $HOME/singlemodes/deltafunctions_ics_test.conf ./
 $HOME/music/MUSIC deltafunctions_ics_test.conf > music.out 2>&1
+mv ic.enzo/* ./
+rmdir ic.enzo
 
 ## submit enzo job
 cp $HOME/singlemodes/SingleModeCosmology.enzo ./
